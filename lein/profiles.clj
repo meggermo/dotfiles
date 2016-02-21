97f6a4a2c4c68d5f3046115e31af5139f05b708e
Michiel Eggermont <michiel.eggermont@gmail.com>
2016-02-21 11:42:49 +0100

    Update plugins and remove unused ones

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,13 +1,5 @@
 {:user
   {:plugins
-    [[lein-midje "3.1.3"]
-     [com.palletops/pallet-lein "0.6.0-beta.9"]
-     [lein-marginalia "0.7.1"]
-     [lein-difftest "2.0.0"]
-     [cider/cider-nrepl "0.6.0"]
-     [lein-ancient "0.6.7"]
-     ]
-   :dependencies
-    [[alembic "0.2.0"]]
-   }
-}
+    [[lein-midje "3.2"]
+     [lein-marginalia "0.8.0"]
+     [lein-ancient "0.6.8"]]}}
