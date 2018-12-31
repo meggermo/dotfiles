ae26ed75e76498a20d86d20238ab837a83691f76
Michiel Eggermont <michiel.eggermont@gmail.com>
2018-12-31 15:58:46 +0100

    update plugins

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -2,4 +2,5 @@
   {:plugins
     [[lein-midje "3.2"]
      [lein-marginalia "0.8.0"]
-     [lein-ancient "0.6.8"]]}}
+     [lein-ancient "0.6.15"]
+     [com.jakemccrary/lein-test-refresh "0.23.0"]]}}
